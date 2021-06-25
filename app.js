a7134e5ea8b599768ffa1be93dbcb5c260d808c5
lakshmi <lakshmi.mcahbti@gmail.com>
2021-06-25 01:29:03 -0500

    added procfile .gitignore

--- app.js
+++ app.js
@@ -120,7 +120,10 @@ app.post("/delete",function(req,res){
     })
   }
 });
-
-app.listen(process.env.PORT||3000,function(){
-  console.log("server running");
+let port=process.env.PORT;
+if(port==null || port==""){
+  port=3000;
+}
+app.listen(port,function(){
+  console.log("server running at "+port);
 })
